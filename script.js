2b1d9cfcfcfb93b25e2d557b0f98dc4861269bb1
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-29 22:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //your JS code here. If required.
-function makeid(l) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    const charactersLength = characters.length;
-    for (let i = 0; i < l; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+function mapletters(word){
+	const lettermap={};
+	for(let i=0;i<word.length;i++)
+		{
+			const letter=word[i];
+			if(!lettermap[letter]){
+				lettermap[letter]=[i];
+			}
+			else{
+				lettermap[letter].push(i);
+			}
+		}
+	return lettermap;
 }
-
-// Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+console.log(mapletters("dodo"));
