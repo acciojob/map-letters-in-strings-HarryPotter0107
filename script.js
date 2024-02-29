4529b029b63b65bb6d7b516534c7d87eb0294309
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-29 21:59:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function makeid(l) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < l; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+// Do not change the code below.
+const l = prompt("Enter a number.");
+alert(makeid(l));
